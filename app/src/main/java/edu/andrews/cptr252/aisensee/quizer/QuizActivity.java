3a6282e6c45f0d287247290185801fa932d391bf
Arivlorn <60198052+Arivlorn@users.noreply.github.com>
2020-04-01 17:17:36 -0500

    Setup project and added/generated JavaDocs.

---
+++ app/src/main/java/edu/andrews/cptr252/aisensee/quizer/QuizActivity.java
@@ -0,0 +1,17 @@
+package edu.andrews.cptr252.aisensee.quizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * The control layer for the main menu of the app.
+ */
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
